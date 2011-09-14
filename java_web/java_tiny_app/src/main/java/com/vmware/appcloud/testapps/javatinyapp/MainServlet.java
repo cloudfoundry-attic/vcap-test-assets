f15ae93febc91c76667a8dc26a1294030c8ac4c0
AB Srinivasan <abyss@b20nine.com>
2011-09-14 16:08:51 -0700

    Moving test assets (the apps used by the tests) of 'vmc' to new repo.
    Change-Id: Ibfde0908cd5e0456258dd7b2c75228dfbb42830c

---
+++ java_web/java_tiny_app/src/main/java/com/vmware/appcloud/testapps/javatinyapp/MainServlet.java
@@ -0,0 +1,31 @@
+package com.vmware.appcloud.testapps.javatinyapp;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public MainServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if ("true".equals(request.getParameter("shutdown"))) {
+			System.exit(1);
+		}
+		response.setContentType("text/plain");
+		response.setStatus(200);
+		PrintWriter writer = response.getWriter();
+		writer.println("It just needed to be restarted!");
+		writer.println("My instance id: " + System.getenv("VMC_APP_ID"));
+		writer.println("My location: " + System.getenv("VMC_DEA_HOST") + ":" + System.getenv("VMC_DEA_PORT"));
+		writer.close();
+	}
+}

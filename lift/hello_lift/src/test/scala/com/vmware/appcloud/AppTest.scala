aaae0d67081215748d4f74f2a50f7277840b08bc
AB Srinivasan <abyss@b20nine.com>
2011-10-04 16:36:26 -0700

    Update Lift app to Scala 2.8.1 / Lift 2.3.
    Change-Id: I4b879d69be0e7e0a07ebc078ac235c4378e4588f

--- lift/hello_lift/src/test/scala/com/vmware/appcloud/AppTest.scala
+++ lift/hello_lift/src/test/scala/com/vmware/appcloud/AppTest.scala
@@ -5,6 +5,7 @@ import _root_.junit.framework._
 import Assert._
 import _root_.scala.xml.XML
 import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
 
 object AppTest {
   def suite: Test = {
@@ -47,15 +48,24 @@ class AppTest extends TestCase("app") {
       if (file.isDirectory)
         for (f <- file.listFiles) wellFormed(f)
 
-      if (file.isFile && handledXml(file.getName)) {
+      /*
+      if (file.isFile && file.exists && handledXml(file.getName)) {
         try {
-          XML.loadFile(file)
+          import java.io.FileInputStream
+          val fis = new FileInputStream(file)
+          try {
+            XML.load(fis)
+          } finally {
+            fis.close()
+          }
         } catch {
           case e: _root_.org.xml.sax.SAXParseException => failed = file :: failed
         }
       }
-      if (file.isFile && handledXHtml(file.getName)) {
-        PCDataXmlParser(new java.io.FileInputStream(file.getAbsolutePath)) match {
+      */
+
+      if (file.isFile && file.exists && handledXHtml(file.getName)) {
+        PCDataXmlParser(new _root_.java.io.FileInputStream(file.getAbsolutePath)) match {
           case Full(_) => // file is ok
           case _ => failed = file :: failed
         }

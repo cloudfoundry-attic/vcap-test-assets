c0e1330d99e4c8f929394530d615bfd38a1a9fe8
AB Srinivasan <abyss@b20nine.com>
2011-10-04 16:36:26 -0700

    Update Lift app to Scala 2.8.1 / Lift 2.3.
    Change-Id: I4b879d69be0e7e0a07ebc078ac235c4378e4588f

--- lift/hello_lift/src/main/scala/com/vmware/appcloud/snippet/HelloWorld.scala
+++ lift/hello_lift/src/main/scala/com/vmware/appcloud/snippet/HelloWorld.scala
@@ -1,6 +1,25 @@
-package com.vmware.appcloud.snippet
+package com.vmware.appcloud {
+package snippet {
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import com.vmware.appcloud.lib._
+import Helpers._
 
 class HelloWorld {
-  def howdy = <span>Welcome to scala_lift at {new _root_.java.util.Date}</span>
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+
+  // bind the date into the element with id "time"
+  def howdy = "#time *" #> date.map(_.toString)
+
+  /*
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+   def howdy = "#time *" #> date.toString
+   */
 }
 
+}
+}

42ee896ea7a92f662209f98105bba3ec418faa59
Jennifer Hickey <jhickey@vmware.com>
2011-10-10 16:29:28 -0700

    Test disallow of eager bean instantiation during auto-config scan
    - Add test to verify no eager instantiation
    of FactoryBeans during auto-config scan
    
    Change-Id: I2c84738e43985843b1380107c559346797724b86

---
+++ spring/auto-reconfig-test-app/src/main/java/org/cloudfoundry/test/FakeFactoryBeanDependency.java
@@ -0,0 +1,13 @@
+package org.cloudfoundry.test;
+import org.springframework.stereotype.Component;
+
+/**
+ * Fake auto-wired dependency for {@link FakeFactoryBean}
+ * @author Jennifer Hickey
+ *
+ */
+@Component
+public class FakeFactoryBeanDependency {
+
+}
+

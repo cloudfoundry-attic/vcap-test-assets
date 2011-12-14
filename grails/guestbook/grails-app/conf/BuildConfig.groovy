5e90889cd5a1d343364dd6e6a2eaf27747c82756
AB Srinivasan <abyss@b20nine.com>
2011-12-13 16:07:20 -0800

    Update grails app config to make 'mvn' based builds self-contained i.e does not require installation of grails.
    Change-Id: Ib00ebccc5d352f9dfc658d1fdb87d7c571178fff

--- grails/guestbook/grails-app/conf/BuildConfig.groovy
+++ grails/guestbook/grails-app/conf/BuildConfig.groovy
@@ -14,18 +14,24 @@ grails.project.dependency.resolution = {
         grailsHome()
         grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        // Enable remote dependency resolution from public Maven repositories
+        // - helps resolve hibernate / tomcat plugins and associated dependencies
+        mavenLocal()
+        mavenCentral()
+        mavenRepo "http://snapshots.repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://download.java.net/maven/2/"
+        mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
+}
+// Squash the use of the outdated 1.5.2 version of slf4j - use the version
+// specified (1.5.8) in the pom.xml for the app.
+grails.war.resources = { stagingDir ->
+   delete(file:"${stagingDir}/WEB-INF/lib/slf4j-api-1.5.2.jar")
+   delete(file:"${stagingDir}/WEB-INF/lib/commons-collections.jar")
 }

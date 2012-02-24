bd189fa7e27cba6216b63b1e23b382c55a28ec30
SonicWang <wangs@vmware.com>
2012-02-24 01:22:20 -0800

    Revive S3 interface for test app: Revert "update blob test app; removed s3 gems; use net-http-digest_auth (MIT licensed)"
    This reverts commit 28636edc586d60b74908be59670de0d19bbaa87f.

--- sinatra/blob_app/blob.rb
+++ sinatra/blob_app/blob.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'uri'
+require 'aws/s3'
 require 'json'
-require 'net/http'
-require 'net/http/digest_auth'
+
 services = JSON.parse(ENV['VCAP_SERVICES'])
 blob_gw = nil
 services.each do |k,v|
@@ -13,33 +13,15 @@ services.each do |k,v|
   end
 end
 
-user = blob_gw[0]['credentials']['username']
-passwd = blob_gw[0]['credentials']['password']
-dest_port = blob_gw[0]['credentials']['port']
-dest_host = blob_gw[0]['credentials']['host']
-
-def make_request(dest_host, dest_port, user, passwd, method, resource, content)
-  uri = URI.parse "http://#{dest_host}:#{dest_port}/#{resource}"
-  uri.user = user
-  uri.password = passwd
-  h = Net::HTTP.new uri.host, uri.port
-  req = Net::HTTP::Get.new uri.request_uri
-  res = h.request req
-  digest_auth = Net::HTTP::DigestAuth.new
-  auth = digest_auth.auth_header uri, res['www-authenticate'], method
-  if method == 'GET'
-    req = Net::HTTP::Get.new uri.request_uri
-  elsif method == 'PUT'
-    req = Net::HTTP::Put.new uri.request_uri
-    if content != nil 
-      req.body_stream=content
-      req.add_field 'content-length', content.length
-    end
-  elsif method == 'DELETE'
-    req = Net::HTTP::Delete.new uri.request_uri
-  end
-  req.add_field 'Authorization', auth
-  res = h.request req
+begin
+AWS::S3::Base.establish_connection!(
+    :access_key_id     => blob_gw[0]['credentials']['username'],
+    :secret_access_key => blob_gw[0]['credentials']['password'],
+    :port => blob_gw[0]['credentials']['port'],
+    :server => blob_gw[0]['credentials']['host']
+  ) unless blob_gw == nil
+rescue => e
+  puts "#{e}"
 end
 
 get '/' do
@@ -57,43 +39,63 @@ get '/env' do
 end
 
 get '/list' do
-  res = make_request dest_host, dest_port, user, passwd, 'GET', "", nil
-  status res.code
-  body res.body
+  if blob_gw == nil
+    return
+  end
+  buckets = AWS::S3::Service.buckets(:reload)
+  buckets.inspect
 end
 
-post '/:container' do
-  res = make_request dest_host, dest_port, user, passwd, 'PUT', "#{params[:container]}", nil
-  status res.code
-  body res.body
+post '/:bucket' do
+  begin
+    rc = AWS::S3::Bucket.create params[:bucket]
+    rc
+  rescue => e
+    e.inspect
+  end
 end
 
-get '/:container' do
-  res = make_request dest_host, dest_port, user, passwd, 'GET', "#{params[:container]}", nil
-  status res.code
-  body res.body
+get '/:bucket' do
+  begin
+    buck = AWS::S3::Bucket.find params[:bucket]
+    buck.inspect
+  rescue => e
+    e.inspect
+  end
 end
 
-delete '/:container' do
-  res = make_request dest_host, dest_port, user, passwd, 'DELETE', "#{params[:container]}", nil
-  status res.code
-  body res.body
+delete '/:bucket' do
+  begin
+    rc = AWS::S3::Bucket.delete params[:bucket]
+    rc
+  rescue => e
+    e.inspect
+  end
 end
 
-post '/:container/:file' do
-  res = make_request dest_host, dest_port, user, passwd, 'PUT', "#{params[:container]}/#{params[:file]}", request.body
-  status res.code
-  body res.body
+post '/:bucket/:object' do
+  begin
+    rc = AWS::S3::S3Object.store(params[:object],request.body,params[:bucket])
+    rc
+  rescue => e
+    e.inspect
+  end
 end
 
-get '/:container/:file' do
-  res = make_request dest_host, dest_port, user, passwd, 'GET', "#{params[:container]}/#{params[:file]}", nil
-  status res.code
-  body res.body
+get '/:bucket/:object' do
+  begin
+    content = AWS::S3::S3Object.value params[:object],params[:bucket]
+    content
+  rescue => e
+    e.inspect
+  end
 end
 
-delete '/:container/:file' do
-  res = make_request dest_host, dest_port, user, passwd, 'DELETE', "#{params[:container]}/#{params[:file]}", nil
-  status res.code
-  body res.body
+delete '/:bucket/:object' do
+  begin
+    rc = AWS::S3::S3Object.delete params[:object],params[:bucket]
+    rc
+  rescue => e
+    e.inspect
+  end
 end

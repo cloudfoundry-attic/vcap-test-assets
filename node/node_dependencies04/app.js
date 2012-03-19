ea30204f02d366b4b72ee087dddae27250234c9c
Maria Shaldybina <mariash@vmware.com>
2012-03-18 20:39:08 -0700

    Node.js apps with specified dependencies
    Change-Id: Id05832eb04f46ec5fcfe79058170df420f8b9513

---
+++ node/node_dependencies04/app.js
@@ -0,0 +1,17 @@
+var app = require('express').createServer();
+var bcrypt = require('bcrypt');
+
+app.get('/', function(req, res) {
+  var salt = bcrypt.gen_salt_sync(10);
+  var hash = bcrypt.encrypt_sync("B4c0/\/", salt);
+  if (bcrypt.compare_sync("B4c0/\/", hash))
+    res.send('hello from express');
+  else
+    res.send('');
+});
+
+var port = process.env.VCAP_APP_PORT || 3000
+app.listen(port);
+
+console.log(app.address());
+console.log('Express server started on port %s', app.address().port);
---
+++ node/node_dependencies06/app.js
@@ -0,0 +1,17 @@
+var app = require('express').createServer();
+var bcrypt = require('bcrypt');
+
+app.get('/', function(req, res) {
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync("B4c0/\/", salt);
+  if (bcrypt.compareSync("B4c0/\/", hash))
+    res.send('hello from express');
+  else
+    res.send('');
+});
+
+var port = process.env.VCAP_APP_PORT || 3000
+app.listen(port);
+
+console.log(app.address());
+console.log('Express server started on port %s', app.address().port);

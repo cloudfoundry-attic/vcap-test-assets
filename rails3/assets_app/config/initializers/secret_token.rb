8d07db7e86af3ef559a375e29dd70bc9c161390d
Maria Shaldybina <mariash@vmware.com>
2012-10-17 16:19:34 -0700

    App to test rails 3 assets:precompile
    Change-Id: I74cfca781dd61369fd97e45aee05bf80f45b0f9e

---
+++ rails3/assets_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetsApp::Application.config.secret_token = 'eed133f7670c62c16524295f62fab3c6067e341420e8339becb02296dbdf4dd4c21757042a851f9998e59faf1d41034d3e2514ad4792fff3c07082bb83a1968f'

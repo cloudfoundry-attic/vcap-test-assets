3d998dde5485a988f50a0fa0088f0f1750633ed8
Bob Nugmanov and Dmitriy Kalinin <pair+bob+dmitriy@pivotallabs.com>
2013-03-27 15:06:34 -0700

    removed unused node apps

--- node/node_npm/app.js
+++
@@ -1,8 +0,0 @@
-
-var app = require('express').createServer();
-
-app.get('/', function(req, res){
-    res.send('hello world test using express and npm!');
-});
-
-app.listen(process.env.VCAP_APP_PORT || 3000);

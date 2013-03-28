9bf3bfbdd3b5dfb6b490908cb4d945c152b4b1e7
Bob Nugmanov and Dmitriy Kalinin <pair+bob+dmitriy@pivotallabs.com>
2013-03-27 19:06:48 -0700

    cleaned up node0_6 app to include native and git deps

---
+++ node/node0_6/app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express.createServer();
+
+app.get("/node_version", function(req, res){
+  res.send(process.version);
+});
+
+app.get("/git_module", function(req, res){
+  // Tested: git module support
+  var semver = require("semver");
+
+  if (semver && semver.valid("1.2.3")) {
+    res.send("ok");
+  } else {
+    res.send("fail");
+  }
+});
+
+app.get("/native_ext", function(req,res) {
+  // Tested: native extensions via node-gyp
+  var bcrypt = require("bcrypt");
+
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync("B4c0/\/", salt);
+
+  if (bcrypt.compareSync("B4c0/\/", hash)) {
+    res.send("ok");
+  } else {
+    res.send("fail");
+  }
+});
+
+app.listen(process.env.VCAP_APP_PORT || 3000);

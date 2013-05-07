ad0f423a733d7fa2ac0eae1cfc27e0c321e00b95
David Stevenson and Jesse Zhang <pair+stevend+jesse@pivotallabs.com>
2013-05-07 15:44:14 -0700

    happy path and totally sad path

---
+++ sinatra/service_pinger/spec/db_verifier_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+require_relative '../lib/db_verifier'
+
+describe DbVerifier do
+  let(:db_name) { "service_pinger_test" }
+  let(:root_connection) { Sequel.connect "mysql2://root@localhost" }
+  before(:each) do
+    root_connection.run "CREATE DATABASE IF NOT EXISTS #{db_name}"
+    root_connection.run "GRANT ALL ON *.* TO 'testuser'@'localhost'"
+  end
+
+  describe "#db_connect_string" do
+    it 'should find the first service' do
+      ENV["VCAP_SERVICES"] =<<JSON
+{"rds_mysql-n/a":[{"name":"rds_pinger","label":"rds_mysql-n/a","plan":"10mb","credentials":{"name":"dbname","hostname":"dbhost.rds.amazonaws.com","host":"dbhost.rds.amazonaws.com","port":3306,"user":"u","username":"u","password":"p"}}]}
+JSON
+      described_class.db_connect_string.should eq('mysql2://u:p@dbhost.rds.amazonaws.com:3306/dbname')
+    end
+
+    it 'should fail when there are no services' do
+      ENV["VCAP_SERVICES"] = "{}"
+      expect {
+        described_class.db_connect_string
+      }.to raise_error(DbVerifier::ExpectedOneDatabaseError)
+    end
+  end
+
+  describe "#metrics" do
+    context "when the connect, read, and write all work" do
+      before do
+        ENV["VCAP_SERVICES"] =<<JSON
+{"_":[{"name":"_name","label":"rds_mysql-n/a","plan":"10mb","credentials":{"name":"#{db_name}","hostname":"localhost","host":"localhost","port":3306,"user":"testuser","username":"testuser","password":""}}]}
+JSON
+      end
+
+      it "returns metrics" do
+        v = described_class.new
+        results = v.metrics
+
+        [:connect, :write, :read].each do |mname|
+          result = results[mname]
+          result.keys.should =~ [:latency, :error]
+          result[:error].should be_nil
+          result[:latency].should be_a Numeric
+        end
+      end
+    end
+
+    context "when the database cannot be connected to" do
+      before do
+        ENV["VCAP_SERVICES"] = <<JSON
+{"_":[{"name":"_name","label":"rds_mysql-n/a","plan":"10mb","credentials":{"name":"#{db_name}","hostname":"localhost","host":"localhost","port":33306,"user":"wrongroot","username":"wrongroot","password":"wrong"}}]}
+JSON
+      end
+
+      it "returns connect failure info" do
+        v = described_class.new
+        v.metrics.keys.should =~ [:connect]
+        result = v.metrics[:connect]
+        result.keys.should =~ [:latency, :error]
+        result[:latency].should == -1
+        result[:error].should =~ /Access denied/
+      end
+    end
+  end
+end

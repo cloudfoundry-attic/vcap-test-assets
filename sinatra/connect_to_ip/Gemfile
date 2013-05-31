53315296b38e5f854bb21e32fdfc3821999497ef
Kowshik Prakasam and Mark Rushakoff <pair+kowshik+mr@pivotallabs.com>
2013-05-31 14:09:12 -0700

    Add sinatra app that connects to IP/port

---
+++ sinatra/connect_to_ip/Gemfile
@@ -0,0 +1,11 @@
+source "http://rubygems.org"
+# Without any bound services, app runs on sqlite3
+#.. but may also be staged with mysql
+gem 'mysql2'
+gem 'mongo'
+gem 'redis'
+gem 'sinatra'
+gem 'json'
+gem 'carrot'
+gem 'pg'
+gem 'aws-s3'
---
+++ sinatra/connect_to_ip/main.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'socket'
+require 'timeout'
+
+get '/:ip/:port' do |ip, port|
+  if is_port_open?(ip, port)
+    "#{ip}:#{port} is open"
+  else
+    "#{ip}:#{port} is NOT open"
+  end
+end
+
+def is_port_open?(ip, port)
+  begin
+    Timeout::timeout(1) do
+      begin
+        s = TCPSocket.new(ip, port)
+        s.close
+        return true
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+        return false
+      end
+    end
+  rescue Timeout::Error
+  end
+
+  return false
+end

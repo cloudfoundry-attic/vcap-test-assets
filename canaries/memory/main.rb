ab80e6a359de890d1961d9bbc99d2b4e13bbb01f
Alan Moran and Alex Choi <pair+bonzofenix+alex@pivotallabs.com>
2013-07-30 17:26:01 -0700

    fixing broken port setting

--- canaries/memory/main.rb
+++ canaries/memory/main.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require_relative "lib/memory_kudzu"
 
 MEMORY = ENV["MEMORY"] ? ENV["MEMORY"].to_i : 120
-set :port, ENV["PORT"] ? ENV["PORT.to_i"] : 3000
+set :port, ENV["PORT"] ? ENV["PORT"].to_i : 3000
 
 MemoryKudzu.grow_until(MEMORY)
 
